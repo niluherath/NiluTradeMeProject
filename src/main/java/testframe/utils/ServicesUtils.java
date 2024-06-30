221ca601583268b9d3b3670f57f1688758bc878f
cool <niluherath@hotmail.com>
2024-07-01 09:44:41 +1200

    api tests are working

--- src/main/java/testframe/utils/ServicesUtils.java
+++ src/main/java/testframe/utils/ServicesUtils.java
@@ -6,14 +6,16 @@ import io.restassured.specification.RequestSpecification;
 
 public class ServicesUtils {
 
-    public static Response getNoOfFerrariMakes(){
-        RestAssured.baseURI = "https://trademe.co.nz";
+    public static Response getNoOfCarMakes(String carMake){
+        RestAssured.baseURI = "https://api.trademe.co.nz";
         RequestSpecification request = RestAssured.given().relaxedHTTPSValidation();
         request.header("accept","*/*" );
         request.header("accept-encoding:","gzip, deflate, br" );
         request.header("accept-language","en-US,en;q=0.9" );
         request.header("access-control-request-headers"," authorization,newrelic,x-trademe-uniqueclientid" );
-        Response response = request.get("/a/motors/cars/ferrari/search?auto_category_jump=false");
+       // Response response = request.get("v1/searchoptions.json?dependent_value=Ferrari&key=used_car_model");
+        Response response = request.get("v1/searchoptions.json?dependent_value="+carMake+"&key=used_car_model");
+
         return response;
 
 
--- src/main/java/trademetests/API_test/TestUsedCarsCategories.java
+++ src/main/java/trademetests/API_test/TestUsedCarsCategories.java
@@ -1,16 +1,56 @@
 package trademetests.API_test;
 
+import io.restassured.path.json.JsonPath;
 import testframe.utils.ServicesUtils;
 import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 public class TestUsedCarsCategories {
 
-    @Test
-    public void testUsedCarsCategories(){
-        // https://www.trademe.co.nz/a/motors/cars/ferrari/search?auto_category_jump=false
-        Response response = ServicesUtils.getNoOfFerrariMakes();
+    @org.testng.annotations.Test
+    public void testFerrariNoOfCars(){
+        Response response = ServicesUtils.getNoOfCarMakes("Ferrari");
         Assert.assertEquals(response.statusCode(), 200);
+        JsonPath jsonPathEvaluator = response.jsonPath();
+        List<String>  carMakes = jsonPathEvaluator.getList("");
+        System.out.println("Ferrari no of Cars: "+ carMakes.size());
+       // return String.valueOf(carMakes.size());
+
+    }
+
+    @org.testng.annotations.Test
+    public void testBMWNoOfCars(){
+        Response response = ServicesUtils.getNoOfCarMakes("BMW");
+        Assert.assertEquals(response.statusCode(), 200);
+        JsonPath jsonPathEvaluator = response.jsonPath();
+        List<String>  carMakes = jsonPathEvaluator.getList("");
+        System.out.println("BMW no of Cars: "+ carMakes.size());
+       // return String.valueOf(carMakes.size());
+
+    }
+
+    @org.testng.annotations.Test
+    public void testHondaNoOfCars(){
+        Response response = ServicesUtils.getNoOfCarMakes("Honda");
+        Assert.assertEquals(response.statusCode(), 200);
+        JsonPath jsonPathEvaluator = response.jsonPath();
+        List<String>  carMakes = jsonPathEvaluator.getList("");
+        System.out.println("Honda no  of Cars: "+ carMakes.size());
+      //  return String.valueOf(carMakes.size());
+
+    }
+
+    @org.testng.annotations.Test
+    public void testMazdaNoOfCars(){
+        Response response = ServicesUtils.getNoOfCarMakes("Mazda");
+        Assert.assertEquals(response.statusCode(), 200);
+        JsonPath jsonPathEvaluator = response.jsonPath();
+        List<String>  carMakes = jsonPathEvaluator.getList("");
+        System.out.println("Mazda no of Cars: "+ carMakes.size());
+      //  return String.valueOf(carMakes.size());
+
     }
 }
